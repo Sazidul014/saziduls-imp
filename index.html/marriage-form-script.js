5a5cd31bae2c64e43460e29137b4b13388a53637
Sazidul014 <sazidullaskar618@gmail.com>
2024-11-15 18:47:30 +0530

    Add files via upload

---
+++ index.html/marriage-form-script.js
@@ -0,0 +1,5 @@
+document.getElementById("marriageForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents the form from submitting the default way
+    alert("Your application for Ekram's marriage form has been submitted successfully!");
+    // Code to handle form data submission can be added here
+});
